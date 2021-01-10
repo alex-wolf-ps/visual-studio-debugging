777bb3ec8ab435db16331633c9edb9e8ae73478d
Alex Wolf <alexrudiwolf@gmail.com>
2021-01-10 17:21:01 -0500

    upgrade

--- LoanCalculator.RazorPages/Startup.cs
+++ LoanCalculator.RazorPages/Startup.cs
@@ -35,12 +35,11 @@ namespace LoanCalculator.RazorPages
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
             var connectionString = "DataSource=:memory:";
             services.ConfigureSqlLiteDatabase(connectionString);
             services.ConfigureRepositories();
+
+            services.AddRazorPages();
 
             var rules = new List<ILoanQualificationRule>()
             {
@@ -54,24 +53,20 @@ namespace LoanCalculator.RazorPages
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(ep =>
+            {
+                ep.MapDefaultControllerRoute();
+                ep.MapRazorPages();
+            });
+
         }
     }
 }
